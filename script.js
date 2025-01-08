3cf7e6a2a481bc6dca4309925aed45df5c81a86b
Shreyansh2025 <shreyanshsurana100@gmail.com>
2025-01-08 19:09:02 +0530

    Add new file and modify

---
+++ script.js
@@ -0,0 +1,11 @@
+
+    function toggleContent(button) {
+        const content = button.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+            button.textContent = "Read More";
+        } else {
+            content.style.display = "block";
+            button.textContent = "Read Less";
+        }
+    }
